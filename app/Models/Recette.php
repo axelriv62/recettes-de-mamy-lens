fa368be5189ee26b3514a3d82e08a73075ed4f51
axelriv62 <axel_riviere@ens.univ-artois.fr>
2024-11-06 15:22:53 +0100

    #1 - Réponse à la question 4.2.3 - Créez la classe modèle Recette

---
+++ app/Models/Recette.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recette extends Model {
+    use HasFactory;
+
+    /*
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'nom',
+        'description',
+        'categorie',
+        'visuel',
+        'nb_personnes',
+        'temps_preparation',
+        'cout'
+    ];
+}
