2f473a1039bc57e452a12e69916d27c65cf33363
axelriv62 <axel_riviere@ens.univ-artois.fr>
2024-11-18 16:41:58 +0100

    Réponse à la question 2.1 - Insertion d'une image et du css grace à Vite

--- resources/views/statiques/accueil.blade.php
+++ resources/views/statiques/accueil.blade.php
@@ -4,4 +4,5 @@
         <h2>Les recettes de Mamy Lens</h2>
         <p>Bienvenue dans l'application des recettes de Mamy Lens.</p>
     </div>
+    <img src="{{ Vite::asset('resources/images/recettes.jpg') }}" alt="Banniere">
 </x-app>
